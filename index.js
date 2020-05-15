95241da3f662d261d3ef20a64fee22da3bef1c5f
pthulen <pfthulen@gmail.com>
2020-05-15 10:22:00 -0700

    server index.js changes

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+
+if (process.env.NODE_ENV === "production") {
+    // express will serve up production assets
+    app.use(express.static('client/build'));
+
+    //serves index.html for unknown routes
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    }); 
+} 
  
 
 const PORT = process.env.PORT || 5000;
